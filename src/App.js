b8c3ab04b1eb16e5541bf80773a097502f05af6a
Raghav <raghav4u03@gmail.com>
2023-08-22 16:52:38 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import Signin from './Components/Signin'
+import HomePage from './Components/HomePage';
+import Signup from './Components/Signup'
+import { AuthContextProvider } from './Components/Context/AuthContext';
+import ProtectedRoutes from './Components/ProtectedRoutes';
+
+function App() {
+  return (
+    <div className=''>
+
+      <AuthContextProvider>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={
+          
+          <ProtectedRoutes>
+          <HomePage /> 
+          </ProtectedRoutes>
+        }>
+            
+
+        </Route>
+      </Routes>
+      </BrowserRouter>
+
+      </AuthContextProvider>
+
+    </div>
+  );
+}
+
+export default App;
+ 
